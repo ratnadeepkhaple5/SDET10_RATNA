4ca1c695db860f19b364f3d2e9a3137288f86339
ratnadeep <ratnadeepkhaple5@gmail.com>
2022-11-15 18:03:30 +0530

    amazon

--- src/test/java/practice/Amazon.java
+++ src/test/java/practice/Amazon.java
@@ -5,12 +5,14 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Amazon {
 
-	public static void main(String[] args) {
+	@Test
+	public void main() {
 		// TODO Auto-generated method stub
 
 		WebDriverManager.chromedriver().setup();
